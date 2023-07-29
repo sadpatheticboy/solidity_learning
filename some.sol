65c4ef18b6384a1e8499c42db38c20f156e9edc3
sadpatheticboy <bal-tim@mail.ru>
2023-07-29 13:24:31 +0300

    data types: bool, uint, int

---
+++ some.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Some {
+    // unsigned integers
+    uint public myUint = 42; // uint = uint256 (2 ** 256)
+    uint8 public mySmallUint = 2; // min=0 -> max=(2 ** (256-1))
+    // uint16 -> uint24 -> uint(+8) ... -> uint256
+
+    // signed integers
+    int public myInt = -42; // int = int256
+    int8 public mySmallInt = -1; // min=-128 -> max=127
+
+    // uint8 public myVal = 255;
+    uint8 public myVal = 1;
+
+    function dec() public {
+        unchecked {
+            // myVal++;
+            myVal--;
+        }
+    }
+
+    // function Demo(uint _inputUnit) public {
+    //     uint localUint = 42;
+    // }
+
+        // bool public myBool = true; // state
+
+        // function myFunc(bool _inputBool) public {
+        //     bool localBool = false;
+        //     localBool && _inputBool; // and
+        //     localBool || _inputBool; // or (lazy (short-curcuit))
+        //     localBool == _inputBool; // equal
+        //     localBool != _inputBool; // not equal
+        //     !localBool; // inversion 
+
+        // }
+}
