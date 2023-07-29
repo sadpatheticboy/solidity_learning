071ba6ded1b83db95160aa025b4614a827ce498d
sadpatheticboy <bal-tim@mail.ru>
2023-07-29 14:07:42 +0300

    data types: string, address, mapping

--- DataTypes2.sol
+++ DataTypes2.sol
@@ -3,5 +3,30 @@
 pragma solidity ^0.8.0;
 
 contract DataTypes2 {
+    // mapping
+    mapping (address => uint) public payments; // storage
+
+    // address
+    address public myAddr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // storage
+
+    function receiveFunds() public payable {
+        payments[msg.sender] = msg.value;
+    }
     
-}
+    function transerTo(address targetAddr, uint amount) public {
+        address payable _to = payable(targetAddr);
+        _to.transfer(amount);
+    }
+    
+    function getBalance(address targetAddr) public  view returns(uint){
+        return targetAddr.balance;
+    }
+
+    // string
+    string public myStr = "test"; // storage
+
+    function demo(string memory newValueStr) public {
+        string memory myTempStr = "temp"; 
+        myStr = newValueStr;
+    }
+}  
--- IntroShopContract.sol
+++ IntroShopContract.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-contract Shop {
+contract IntroShopContract {
 
     // 0xd9145CCE52D386f254917e481eB44e9943F39138
     address public owner;
